ebb80e472c26e00615afac94f19c5b2d4265140a
charly <ch4rly@riseup.net>
2018-11-08 15:30:46 +0100

    fixed package naming

--- src/actions/sso/index.ts
+++ src/actions/sso/index.ts
@@ -1,15 +1,7 @@
 import { Dispatch, AnyAction } from 'redux'
-// import { navigationActions } from 'src/actions/'
 
 export const startSSO = async () => {
-
-  fetch('http://192.168.2.105:9000/credentialRequest')
-  .then((jwt) => {
-    console.log(jwt, 'here is the response')
-  })
-
   return (dispatch: Dispatch<AnyAction>) => {
-    // navigationActions.handleDeepLink()
   }
 }
 
